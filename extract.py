272544d4c5e74cb3ca85832ca63fa7e73079a1d5
Miguel Angel R <migrmrz@gmail.com>
2020-12-14 17:45:07 -0600

    Added Google Sheets functions
    Added functions to create and insert the extrated data into a google sheet. Added minor adjustments on how empty data is showed on dict structure

--- extract.py
+++ extract.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+from googleapiclient.discovery import build
+import pickle
+import datetime
+from pprint import pprint
 
 
 # Extraction of shelter links from all states
@@ -29,6 +33,8 @@ def extract_shelter_details(shelter_urls: list) -> list:
         # address, phone and email
         shelter_dict = {}
         for shelter_html_data in shelter_html_list:
+            if shelter_html_data.select("a")[0].get_text() == "here":
+                continue
             name = shelter_html_data.select("a")[0].get_text()
             shelter_dict['name'] = name
             url = shelter_html_data.select("a")[0].get("href")
@@ -37,20 +43,81 @@ def extract_shelter_details(shelter_urls: list) -> list:
             if "Email" in shelter_text:
                 shelter_text, email = shelter_text.split(" Email:")
                 shelter_dict['email'] = email.strip()
+            else:
+                shelter_dict['email'] = ""
             if "Phone" in shelter_text:
                 shelter_text, phone = shelter_text.split(" Phone:")
                 shelter_dict['phone'] = phone.strip()
+            else:
+                shelter_dict['phone'] = ""
             if shelter_text != name:
                 address = shelter_text[len(name):].strip()
                 shelter_dict['address'] = address
+            else:
+                shelter_dict['address'] = ""
             shelters_details.append(shelter_dict)
             shelter_dict = {}
     return shelters_details
 
 
+# Insertion of individual shelter info to Google Sheets
+def create_google_service():
+    """
+        Creates and returns a Google Sheets service from a pickle file
+        previously generated.
+    """
+    with open("token.pickle", "rb") as token:
+        credentials = pickle.load(token)
+    service = build("sheets", "v4", credentials=credentials)
+    return service
+
+
+def insert_data(shelters_details: list) -> dict:
+    """
+        Creates a spreadsheet and then inserts the extracted information
+    """
+    # Creates google sheets service
+    service = create_google_service()
+    date_str = str(datetime.date.today())
+    spreadsheet_name = "Animal-Shelters_{}".format(date_str)
+    spreadsheet_body = {
+        "properties": {
+            "title": spreadsheet_name
+        }
+    }
+    # Creates spreadsheet
+    create_request = service.spreadsheets().create(body=spreadsheet_body)
+    create_response = create_request.execute()
+    spreadsheet_id = create_response['spreadsheetId']
+    # Edits spreadsheet to insert data
+    range_ = "Sheet1!A2:E2"
+    insert_data_option = "INSERT_ROWS"
+    value_input_option = "RAW"
+    values = []
+    temp_row = []
+    for shelter in shelters_details:
+        for row in shelter.values():
+            temp_row.append(row)
+        values.append(temp_row)
+        temp_row = []
+    value_range_body = {
+        "values": values
+    }
+    edit_request = service.spreadsheets().values().append(
+        spreadsheetId=spreadsheet_id,
+        range=range_,
+        valueInputOption=value_input_option,
+        insertDataOption=insert_data_option,
+        body=value_range_body)
+    edit_response = edit_request.execute()
+    pprint(edit_response['updates'])
+    return spreadsheet_name
+
+
 urls = extract_shelter_links()
 details = extract_shelter_details(urls)
-print(len(details))
-
-
-# Insertion of shelter info to Google Sheet
+print("Extracted a total of {} records".format(len(details)))
+print("Initiating insertion to Google Sheets...")
+print("Insertion results:\n")
+insert_data(details)
+print("Done!")
